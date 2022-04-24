3044aacf6a6964c974be3c245450c25b9e7d1b18
ruth-code-prog <ruthanggraeni24@gmail.com>
2022-04-25 05:48:03 +0700

    hotjar get Button Wa

--- src/parts/PageErrorMessage.js
+++ src/parts/PageErrorMessage.js
@@ -3,8 +3,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function PageErrorMessage({
-  title = "Alo Care",
-  body = "download via Play Store",
+  title = "Ethan Shop",
+  body = "via Play Store",
 }) {
   return (
     <section className="mt-28">
@@ -12,6 +12,14 @@ export default function PageErrorMessage({
         <div className="flex flex-col items-center justify-center">
           <div className="w-full md:w-4/12 text-center">
             <h2 className="text-3xl font-semibold mb-6">{title}</h2>
+            <div>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.itenshop"
+                className="bg-blue-400 text-black hover:bg-black hover:text-pink-400 rounded-full px-8 py-3 mt-4 inline-block flex-none transition duration-200"
+              >
+                Download Gratis
+              </a>
+            </div>
             <p className="text-lg mb-12">{body}</p>
             <Link
               to="/"
